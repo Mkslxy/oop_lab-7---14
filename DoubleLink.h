3579b0589a6281f3d5322d80e73564403d05a84d
PEtyrchachny Alexey <leshalol66@gmail.com>
2024-04-29 22:27:21 +0300

    AddLast object of double line

--- DoubleLink.h
+++ DoubleLink.h
@@ -7,7 +7,7 @@ using namespace std;
 template<typename T>
 class DoubleLink : public SingleLink<T> {
 public:
-    void AddFirst(const T& value)  {
+    void AddFirst(const T &value) {
         auto newNode = make_shared<Node<T>>(value);
         if (this->body) {
             newNode->address = this->body;
@@ -17,6 +17,20 @@ public:
         this->size++;
     }
 
+    void AddLast(const T &value) {
+        auto newNode = make_shared<Node<T>>(value);
+        if (!this->body) {
+            this->body = newNode;
+        } else {
+            auto currentNode = this->body;
+            while (currentNode->address) {
+                currentNode = currentNode->address;
+            }
+            currentNode->address = newNode;
+            newNode->tail = currentNode;
+        }
+        this->size++;
+    }
 
 
 };
